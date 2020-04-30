5a988da0d0d31d5d6faa70424ae6b78ffd87ecde
Mio Akiyama <61921875+Saphielle@users.noreply.github.com>
2020-05-01 00:02:12 +0200

    Update nhentai.py

--- cogs/nsfw/nhentai.py
+++ cogs/nsfw/nhentai.py
@@ -62,9 +62,7 @@ class DoujinReader(Paginator):
         
     @button(emoji='📖', position=6)
     async def on_book(self, payload):
-        self.embeds = [discord.Embed(color=self.bot.color) for _ in range(self._max_index)]
         self.contents = self.doujins[self._index]._images
-        await self.ctx.send(self.embeds)
         await self.goto_index('FIRST')
         
     @button(emoji='↩️', position=7)
