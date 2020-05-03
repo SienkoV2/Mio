263a098720e8635716b09072ddb3932d7abe0adc
Mio Akiyama <61921875+Saphielle@users.noreply.github.com>
2020-05-04 00:52:31 +0200

    Update cleverbot.py

--- cogs/fun/cleverbot.py
+++ cogs/fun/cleverbot.py
@@ -29,36 +29,39 @@ __copyright__ = 'Copyright 2020 Saphielle-Akiyama'
 from random import choice
 
 from discord import Message
-from discord.ext.commands import Cog
+from discord.ext.commands import Cog, CooldownMapping, BucketType
 
 import async_cleverbot as ac
 
 from config import TRAVITIA_TOKEN
+
 
 class CleverbotCog(Cog, name='Fun'):
     def __init__(self, bot):
         self.bot = bot
         self.cb_client = ac.Cleverbot(TRAVITIA_TOKEN)
         self.cb_client.set_context(ac.DictContext(self.cb_client))
+        self._cd = CooldownMapping.from_cooldown(1.0, 60.0, BucketType.member)
         
     @Cog.listener()
-    async def on_message(self, msg : Message):        
+    async def on_message(self, msg: Message):        
         ctx = await self.bot.get_context(msg)
+        
         if (msg.author.bot or ctx.command
-            or not (self.bot.user.mentioned_in(msg) or msg.guild is None)):
+            or not (self.bot.user.mentioned_in(msg) or msg.guild is None)
+            or self._cd.get_bucket(msg).update_rate_limit()
+            or msg.mention_everyone):
             return
         
-        self.bot.loop.create_task(ctx.trigger_typing())
-        
-        if msg.mention_everyone: 
-            return
-              
-        cb_ans = await self.cb_client.ask(msg.content, 
-                                          id_=msg.author.id, 
-                                          emotion=choice(tuple(ac.Emotion)))
+        async with ctx.typing():
+            
+            cb_ans = await self.cb_client.ask(msg.content, 
+                                              id_=msg.author.id, 
+                                              emotion=choice(tuple(ac.Emotion)))
         
         f_ans = f"{ctx.author.mention} {cb_ans.text}" if ctx.guild else cb_ans.text
         await ctx.send(f_ans)
+
 
 def setup(bot):
     bot.add_cog(CleverbotCog(bot))
